ca5c6f200fe57279f7a5aaf8f1508d6ea933458c
mobileappdev0110 <dev.coin.developer@gmail.com>
2025-12-09 09:18:31 -0800

    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, X-API-Key');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, X-API-Key, X-Staff-Identifier');
   
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
