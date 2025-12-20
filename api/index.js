4eec3347669e5257a967ec8e97b47837d80f7e71
mobileappdev0110 <dev.coin.developer@gmail.com>
2025-12-20 15:43:08 -0800

    updated server

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function wrapper for Express app
-const app = require('../server.js');
-
-// Export handler for Vercel serverless functions
-// The Express app already has CORS middleware configured
-module.exports = app;
-
